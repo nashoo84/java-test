d29a5dd5c1173ea185a30650309d70b528a96e1d
nashoo84 <nashwankandallan@gmail.com>
2025-05-19 17:08:15 +0200

    "Fixade variabelnamn och småfel i index.js"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return str.length;
 let name = "Nashwan";
 let nameLength = getStringLength(name);
 let namnet = "Nashwan";
-console.log("The name " + nashwan + " has " + nameLength + " characters.");
+console.log("The name " + name + " has " + nameLength + " characters.");
 
 // Exercise 2 – Function that extracts the year from a date string
 function getYearFromDate(dateString) {
@@ -14,7 +14,7 @@ return dateString.substring(0, 4);
 }
 
 let date = "2019-10-14";
-let Year = getYearFromDate(datum);
+let year = getYearFromDate(date);
 console.log("The year is: " + year);
 
 // Exercise 3 – Simple calculator function
